2168ae947f94d7fb4f11c9e4ba5c018301e95edd
Aleksandr Mikhasenko <aleksandr.mihasenko@gmail.com>
2024-03-03 13:43:54 +0100

    Add layout

--- src/utils/chunk/chunk.ts
+++ src/utils/chunk/chunk.ts
@@ -1,3 +1,18 @@
-export function chunk<T>(arg: Array<T>): Array<T> {
-    return [];
+export function chunk<T>(arg: Array<T>, size = 1): Array<T> {
+    if (size === 1) return arg;
+
+    const arrayLength = arg.length;
+
+    if (!arrayLength) return [];
+
+    let index = 0;
+    let result: Array<T> = [];
+
+    while (index < arrayLength) {
+        index++;
+    }
+
+    return result;
 }
+
+chunk(['a', 'b', 'c', 'd'], 2);
